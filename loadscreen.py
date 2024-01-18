59471a6ff9d1c8fc85d100b007800b90011c5fd8
= <hershelthomas@gmail.com>
2024-01-18 11:33:33 +0200

    MAde it so i can display anthing on map with any row or collom

--- loadscreen.py
+++ loadscreen.py
@@ -28,17 +28,17 @@ startScreen = """
     |_______________________________________________________________________________________________|
 """
 credits = """
-                                    TERMINAL DUNGEON GAME:     
-                                     -CREATED BY-HERSHEL THOMAS
-                                     -TESTED BY JACOB THOMAS  
-                                     -ART BY Hershel Thomas   
+TERMINAL DUNGEON GAME:     
+ -CREATED BY-HERSHEL THOMAS
+ -TESTED BY JACOB THOMAS  
+ -ART BY Hershel Thomas   
 """
 settings = """
-            Setting Screen Size Before Playing:
-            -Make sure screen size is set to FULL SCREEN            
-            -Zoom in or out by pressing down (ctrl) and (+ or -)      
-            -Zoom in or out until edge of map ('|') is at the furthest right side of the screen
-            -type in '/clear' at any point during game play to reset screen if messy
+Setting Screen Size Before Playing:
+-Make sure screen size is set to FULL SCREEN            
+-Zoom in or out by pressing down (ctrl) and (+ or -)      
+-Zoom in or out until edge of map ('|') is at the furthest right side of the screen
+-type in '/clear' at any point during game play to reset screen if messy
 """
 GameLoading = """
                          Game Loading
@@ -65,11 +65,10 @@ def printScreen(screen, clear = True):
     for row in screen:
         print(''.join(row))
 #Adds text to overwrite over main screen
-def addLinesToSreen(lines, screen, rowIndex, color='\033[m'):
+def addLinesToSreen(lines, screen, rowIndex=0, colIndex=0, color='\033[m'):
     for i, row in enumerate(lines):
         for j, char in enumerate(row):
-            if j !=4:
-                screen[len(screen) - rowIndex+i][j] = color + char + '\033[0m'
+            screen[len(screen) - rowIndex+i][colIndex+j] = color + char + '\033[0m'
                 
 def createEmptyString(screenList : list):
     emptyString = ''.join(' ' if char != '\n' else '\n' for char in screenList)
@@ -83,13 +82,13 @@ printScreen(startScreenArray)
 waitForInput('')
 
 start_row_index = 10  # Replace with the index of the first row you want to copy to
-addLinesToSreen(creditsArray, startScreenArray, start_row_index-4, '\033[1m\033[90m')
+addLinesToSreen(creditsArray, startScreenArray, start_row_index-4, 36, '\033[1m\033[90m')
 printScreen(startScreenArray)
 waitForInput('')
 
-addLinesToSreen(createArrayinArray(createEmptyString(credits)), startScreenArray, start_row_index-4, '\033[0m')
-addLinesToSreen(settingsArray, startScreenArray, start_row_index, '\033[1;31m')
-addLinesToSreen(["          Press 'Enter' To Continue"], startScreenArray, 2, '\033[1;31m')
+addLinesToSreen(createArrayinArray(createEmptyString(credits)), startScreenArray, start_row_index-4, 36, '\033[0m')
+addLinesToSreen(settingsArray, startScreenArray, start_row_index, 12, '\033[1;31m')
+addLinesToSreen(["Press 'Enter' To Continue"], startScreenArray, 2, colIndex=8, color='\033[1;31m')
 printScreen(startScreenArray)
 waitForInput('')
 time.sleep(1)
